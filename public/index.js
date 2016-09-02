f84f03a1812b7119743723fa940af83838020138
Paul Park <paul.park@sbdinc.com>
2016-09-01 22:55:20 -0400

    nav menu chevrons

--- public/index.js
+++ public/index.js
@@ -1,6 +1,5 @@
 (function() {
 	var httpRequest;
-	var navItems;
 
 	function getNavBarData(url) {
 		httpRequest = new XMLHttpRequest();
@@ -15,31 +14,63 @@
 	function handleNavBarData() {
 		if (httpRequest.readyState === XMLHttpRequest.DONE) {
 			if (httpRequest.status === 200) {
-				formatResponse(JSON.parse(httpRequest.responseText));
+				afterResponse(JSON.parse(httpRequest.responseText));
 			} else {
 				alert(httpRequest.status, ': there was a problem with the request.');
 			}
 		}
 	}
 
-	function formatResponse(items) {
+	function afterResponse(items) {
 		if (typeof items === 'object') {
-			navItems = items.items;
+			populateMenu(items.items);
 		}
 	}
 
 	getNavBarData('api/nav.json');
 
 	document.querySelector('#menuToggle').onclick = toggleMenu;
+	document.querySelector('#menuLinks').onclick = toggleSecondaryMenu;
 
 	function toggleMenu() {
 		document.querySelector('#slideInMenu').classList.toggle('open');
 		document.querySelector('#mainBody').classList.toggle('open');
 		document.querySelector('#menuToggle').classList.toggle('open');
+		document.querySelector('#mask').classList.toggle('hidden');
 	}
 
-	function populateMenu() {
+	function populateMenu(items) {
+		console.log('items: ', items);
+		var ul = '<ul>';
+		forEach(items, function(item) {
+			ul += generateMenuItem(item);
+		});
+		ul += '</ul>';
+		document.querySelector('#menuLinks').innerHTML = ul;
+	}
 
+	function generateMenuItem(item) {
+		if (item.items.length && item.items.length > 0) {
+			var secondary = '<ul class="secondary-links">';
+			forEach(item.items, function(x) {
+				secondary += generateSecondaryItem(x);
+			});
+			secondary += '</ul>';
+			return '<li class="primary-item clickable has-secondary">' + item.label + secondary + '</li>';
+
+		} else {
+			return '<li class="primary-item clickable"><a href="' + item.url + '">' + item.label + '</a></li>';
+		}
+	}
+
+	function generateSecondaryItem(item) {
+		return '<li class="secondary-item"><a href="' + item.url + '">' + item.label + '</a></li>';
+	}
+
+	function toggleSecondaryMenu(e) {
+		e.stopPropagation();
+		e.target.classList.toggle('open');
+		console.log(e.target.classList);
 	}
 
 	// utility functions
